29090124d3bb212007c4c8921e0f2c6b6302c7f5
osj <dhtpwls0170@gmail.com>
2026-08-03 17:42:41 +0900

    ConsolGameProjectDelete

--- ConsolGameProject/CraftEngine/Level/TestLevel.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-
-#include <Level/Level.h>
-
-class TestLevel : public Craft::Level
-{
-public:
-	virtual void OnInitialized() override;
-};
--- ConsolGameProject/Game/Main.cpp
+++
@@ -1,6 +0,0 @@
-﻿//#include <Engine/Engine.h>
-
-int main()
-{
-
-}
