989e954fa6f2d73fcfc9fa59cc597eb51b3e550d
osj <dhtpwls0170@gmail.com>
2026-08-04 16:58:26 +0900

    파일입출력

---
+++ cppBasic/FileIO_C/Main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cstdio>
+
+int main()
+{
+	// 파일 입출력을 위한 변수
+	FILE* file = nullptr;
+
+	// 파일 열기
+	fopen_s(&file, "Test.Txt","rb");
+
+	// 파일 제대로 열었는지 확인
+	if (!file)
+	{
+		std::cout << "파일 열기 실패\n";
+		return 1;
+	}
+
+	// 파일 열기 성공
+	std::cout << "파일 열기 성공\n";
+
+	fclose(file);
+	file = nullptr;
+
+}
