72a9b038603f7bf2ebeafad4dc93b3e4c5c4fb02
osj <dhtpwls0170@gmail.com>
2026-08-06 11:05:40 +0900

    shared포인터

---
+++ cppBasic/SharedPtr/Main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <memory>
+
+class Actor
+{
+public:
+	virtual ~Actor() = default;
+};
+
+class Player : public Actor
+{};
+
+int main()
+{
+	std::shared_ptr<Actor> actor = std::make_shared<Actor>();
+	std::shared_ptr<Player> player = std::make_shared<Player>();
+
+	// shared_ptr은 소유권 공유 가능
+	// 참조(사용) 회수를 추적해야함
+	std::shared_ptr<Player> otherPlayer = player;
+}
